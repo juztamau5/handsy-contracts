aa8f761f4c7db02c7070738db989b1d278a0a87f
TheExpandingPolytope <jesseengerman@hotmail.com>
2023-05-17 01:22:19 -0500

    deploy local contracts

---
+++ deploy/deployLocal.ts
@@ -0,0 +1,85 @@
+import { Wallet, utils } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+import fs from "fs"; 
+import * as secrets from "../secrets.json"; 
+
+// Get private key from the environment variable
+const PRIVATE_KEY: string = secrets.privateKey;
+
+interface DependencyContracts {
+    wETH: string;
+    vault: string;
+    master: string;
+    classicFactory: string;
+    stableFactory: string;
+    router: string;
+    feeManager: string;
+    feeRecipient: string;
+    feeRegistry: string;
+    fowardRegistry: string;
+}
+
+const fetchDependencyContracts = async (): Promise<DependencyContracts> => {
+    const file = fs.readFileSync("./local-dependency-contracts.json");
+
+    if(!file) {
+        throw new Error("Please run `yarn deploy:local` first");
+    }
+
+    const contracts: DependencyContracts = JSON.parse(file.toString());
+
+    if(!contracts) {
+        throw new Error("Please run `yarn deploy:local` first");
+    }
+
+    return contracts;
+}
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the Hands and Bankroll contracts`);
+
+  const wallet = new Wallet(PRIVATE_KEY);
+
+  const deployer = new Deployer(hre, wallet);
+
+  const dependencyContracts = await fetchDependencyContracts();
+    console.log(dependencyContracts);
+
+  // Load the artifact of the Bankroll contract you want to deploy.
+  const bankrollArtifact = await deployer.loadArtifact("Bankroll");
+
+  // Estimate Bankroll contract deployment fee
+  //const bankrollDeploymentFee = await deployer.estimateDeployFee(bankrollArtifact);
+
+  //const parsedBankrollFee = ethers.utils.formatEther(bankrollDeploymentFee.toString());
+  //console.log(`The Bankroll deployment is estimated to cost ${parsedBankrollFee} ETH`);
+
+  //deployer address
+  //const deployerAddress = await wallet.getAddress();
+
+  // Deploy Bankroll contract
+  const bankrollContract = await deployer.deploy(bankrollArtifact);
+
+  // Show the Bankroll contract info
+  const bankrollContractAddress = bankrollContract.address;
+  console.log(`Bankroll was deployed to ${bankrollContractAddress}`);
+
+  // Load the artifact of the Hands contract you want to deploy.
+  const handsArtifact = await deployer.loadArtifact("Hands");
+
+  // Estimate Hands contract deployment fee
+  const handsDeploymentFee = await deployer.estimateDeployFee(handsArtifact, [bankrollContractAddress]);
+
+  const parsedHandsFee = ethers.utils.formatEther(handsDeploymentFee.toString());
+  console.log(`The Hands deployment is estimated to cost ${parsedHandsFee} ETH`);
+
+  // Deploy Hands contract, passing the address of the deployed Bankroll contract to the constructor
+  const handsContract = await deployer.deploy(handsArtifact, [bankrollContractAddress]);
+
+  // Show the Hands contract info
+  const handsContractAddress = handsContract.address;
+  console.log(`Hands was deployed to ${handsContractAddress}`);
+}
+
